ec395638bc225498539e52bc96df9b5a28dc6c72
Aaron Donaldson <atreids@outlook.com>
2022-06-09 19:18:20 +0100

    finished exercises 3.7-3.8, and therefore finished part3a

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const { response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let phonebook = [
   {

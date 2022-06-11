9fdddd6e27390f0e729b52a80936e596b7705d13
Aaron Donaldson <aaron-donaldson@github.com>
2022-06-11 13:54:43 +0100

    prepared backend for deploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { response } = require("express");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);

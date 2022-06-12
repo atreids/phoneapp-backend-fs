e58c7064f8aa27a7a641e2b018c491dff72ce0a9
Aaron Donaldson <aaron-donaldson@github.com>
2022-06-12 19:07:45 +0100

    uibuild

--- index.js
+++ index.js
@@ -96,6 +96,21 @@ app.post("/api/persons/", (req, res, next) => {
   // });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const errorHandler = (error, request, response, next) => {
   console.log(error.message);
 

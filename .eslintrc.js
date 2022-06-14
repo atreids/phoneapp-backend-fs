9c3daf29a2993af3d274eca664db0810dc56be81
Aaron Donaldson <atreids@outlook.com>
2022-06-14 15:00:42 +0100

    added prettier config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
-/* eslint-disable */
-
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
     es2021: true,
-    node: true,
   },
   extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: "latest",
   },
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-undef": 0,
   },
 };
